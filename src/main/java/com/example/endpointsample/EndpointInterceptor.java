8b2ee0066172502908d31964fe682d99f567efb2
ise-208 <seed.0ri00@gmail.com>
2023-07-24 21:08:17 +0900

    preHandleでの出力

--- src/main/java/com/example/endpointsample/EndpointInterceptor.java
+++ src/main/java/com/example/endpointsample/EndpointInterceptor.java
@@ -2,13 +2,20 @@ package com.example.endpointsample;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerFactoryFriend;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 public class EndpointInterceptor implements HandlerInterceptor {
+
+    Logger log = LoggerFactory.getLogger(EndpointInterceptor.class);
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return HandlerInterceptor.super.preHandle(request, response, handler);
+        log.info("preHandle");
+        return true;
     }
 
     @Override
